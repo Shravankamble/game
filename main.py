c7a1bfa71b981b089fdbc2eef68de3a44958b14b
shravan kamble <kambleshravan014@gmail.com>
2022-06-23 22:58:36 +0530

    Need to update README.md

--- main.py
+++ main.py
@@ -13,9 +13,22 @@ pygame.display.set_caption('SpeedRunner')
 pygame_logo = pygame.image.load('run.png')
 pygame.display.set_icon(pygame_logo)
 sky_image = pygame.image.load('sky_image.png')
-airplane = pygame.image.load('airplane.png')
-cloud = pygame.image.load('cloudy.png')
-extra_clouds = pygame.image.load('clouds.png')
+airplane = pygame.image.load('airplane.png').convert_alpha()
+cloud = pygame.image.load('cloudy.png').convert_alpha()
+extra_clouds = pygame.image.load('clouds.png').convert_alpha()
+sun = pygame.image.load('contrast.png').convert_alpha()
+ground = pygame.image.load('ground.png')
+extra_ground = pygame.image.load('extra_ground.png')
+car = pygame.image.load('car.png').convert_alpha()
+tree = pygame.image.load('tree.png').convert_alpha()
+house = pygame.image.load('house.png').convert_alpha()
+family_of_that_house = pygame.image.load('garden.png')
+family_of_that_house_x = 555
+house_x = 565
+tree_x = 5
+ground_y = 500
+car_x = 1300
+sun_x = -10
 extra_clouds_x = 900
 cloud_x = 1100
 airplane_x = 0
@@ -25,9 +38,9 @@ Tick = pygame.time.Clock()
 # aquiring space is (100, 200) for that you use pygame.Surface
 # this is just to have an idea that how work with co-ordinates
 # no need of this just practicing.
-testing = pygame.Surface((100, 200))
+# testing = pygame.Surface((100, 200))
 # the fill is just means adding color to it
-testing.fill('white')
+# testing.fill('white')
 
 # this is the while loop to run display until the user quits means exiting the game.
 while True:
@@ -39,7 +52,7 @@ while True:
     #screen.blit(testing, (10, 500))
     screen.blit(sky_image, (0, 0))
     airplane_x += 1
-    if airplane_x > 1300:
+    if airplane_x > 2000:
         airplane_x = values.airplane_respawn_pos
     screen.blit(airplane, (airplane_x, 120))
     screen.blit(cloud, (cloud_x, 120))
@@ -47,6 +60,25 @@ while True:
     if cloud_x < -185:
         cloud_x = values.cloud_x_respawn_pos
     screen.blit(extra_clouds, (extra_clouds_x, 129))
+    screen.blit(sun, (sun_x, 0))
+    screen.blit(ground, (0, ground_y))
+    screen.blit(extra_ground, (800, ground_y))
+    screen.blit(tree, (tree_x, 380))
+    screen.blit(house, (house_x, 380))
+    screen.blit(family_of_that_house, (family_of_that_house_x, 471))
+    screen.blit(car, (car_x, 420))
+    house_x += 4
+    if house_x > 2000:
+        house_x = values.house_x_respawn_pos
+    family_of_that_house_x += 4
+    if family_of_that_house_x > 2000:
+        family_of_that_house_x = values.family_of_that_house_x_respawn_pos
+    tree_x += 4
+    if tree_x > 5000:
+        tree_x = values.tree_x_respawn_pos
+    car_x -= 4
+    if car_x < -100:
+        car_x = values.car_x_respawn_pos
     extra_clouds_x -= 0.1
     if extra_clouds_x < -200:
         extra_clouds_x = values.extra_cloud_respawn_pos
--- values.py
+++ values.py
@@ -1,6 +1,7 @@
-from numpy import extract
-
-
 airplane_respawn_pos = -100
 cloud_x_respawn_pos = 1400
 extra_cloud_respawn_pos = 1350
+car_x_respawn_pos = 1250
+tree_x_respawn_pos = -30
+house_x_respawn_pos = -200
+family_of_that_house_x_respawn_pos = -200
