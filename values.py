49caa904ec5e89f2b42cec7783031f8ce30e18fa
shravan <87265691+Shravankamble@users.noreply.github.com>
2022-12-22 01:08:45 +0530

    Update values.py

--- values.py
+++ values.py
@@ -7,8 +7,10 @@ house_x_respawn_pos = -100
 family_of_that_house_x_respawn_pos = -100
 player_rect_left_respawn_pos = -100
 
-def hello()
-  return "hello world"
+a = 10
+b = "name"
+c = a + b
+print("sum of two numbers = %v", c)
 
 # airplane_respawn_pos = -100
 # cloud_x_respawn_pos = 1400
