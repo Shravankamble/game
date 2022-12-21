7857f5b58520e9c817b3ff9bd74311eea9865f8c
shravan <87265691+Shravankamble@users.noreply.github.com>
2022-12-22 00:57:59 +0530

    Update values.py

--- values.py
+++ values.py
@@ -7,6 +7,9 @@ house_x_respawn_pos = -100
 family_of_that_house_x_respawn_pos = -100
 player_rect_left_respawn_pos = -100
 
+def hello()
+  return "hello world"
+
 # airplane_respawn_pos = -100
 # cloud_x_respawn_pos = 1400
 # extra_cloud_respawn_pos = 1350
